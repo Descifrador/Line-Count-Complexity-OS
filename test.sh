e937734487950dffd16252854ebc5610bc9efd7a
Avinal <avinal.xlvii@gmail.com>
2020-07-22 13:57:45 +0530

    minor changes

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-function count_lines2() {
+function count_lines() {
     find "$1" | while read FILE; do
         echo $(grep -c ^ <"$FILE") >> comparefile.txt
     done
 }
-count_lines2 $1
+count_lines $1
